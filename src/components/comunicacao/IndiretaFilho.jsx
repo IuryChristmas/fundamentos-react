65e428c710c9f651fdf0602948b3390071b6c42c
Iury Christmas <iury.christmas@gmail.com>
2025-07-09 18:52:02 -0300

    feito aula de componente com estado e controlado

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export default props => {
     const cb = props.clickFilho
+    const gerarIdade = () => Math.floor(Math.random() * (20) + 50)
+    const gerarNerd = () => Math.random() > 0.5
 
     return (
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={_ => cb('Iury', 30, true)}>
+            <button onClick={_ => cb('Iury', gerarIdade(), gerarNerd())}>
                 Fornecer Informações
             </button>
         </div>
